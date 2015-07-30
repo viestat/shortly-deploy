345de04c3222618fa74237f592e28d3a32875156
viestat <viestat@icloud.com>
2015-07-29 18:55:08 -0700

    add routine to Grunt that will run upon deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,10 +140,6 @@ module.exports = function(grunt) {
     'squishAll'
   ]);
 
-  grunt.registerTask('prod', [
-
-  ]);
-
   grunt.registerTask('squishBackbone', [
     'concat:backbone',
     'uglify:backbone'
@@ -156,7 +152,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
-      // add your production server task here
+      grunt.task.run([ 'build' ]);
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
